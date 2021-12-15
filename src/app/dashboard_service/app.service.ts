121c27ab67f3688540389493c20119803f5cf65f
r <riteshpatidar787@gmail.com>
2021-12-15 14:43:53 +0530

    Course api integrated

--- src/app/dashboard_service/app.service.ts
+++ src/app/dashboard_service/app.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders} from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { GlobalConstants } from '../global-constants';
@@ -38,5 +38,13 @@ export class AppService {
     body.append("content", JSON.stringify(data.content));
     body.append("course_file",data.course_file, data.course_file.name)
     return this.http.post(this.apiUrl+"api/admin/addCourse",body,this.httpOptionsFormData);
+  }
+
+  getAllCourses():Observable<any>{
+    return this.http.get(this.apiUrl+"api/allCourses", this.httpOptionsUrlEncoded);
+  }
+
+  deleteCouse(id:string):Observable<any>{
+    return this.http.delete(this.apiUrl+"api/admin/deleteCourse/"+id,this.httpOptionsUrlEncoded);
   }
 }
